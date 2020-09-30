f50e5ea3c2fe8a1469b27c5c5bd95e843e30718b
kunming100 <kim_tankunming@outlook.com>
2020-09-30 16:53:27 +0800

    修改部分配置

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Button } from 'antd'
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>click me</Button>
       </header>
     </div>
   );
