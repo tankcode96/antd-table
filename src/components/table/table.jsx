96a9f25f1e24e497afff7c21ec1f7969b7e0f439
kunming100 <kim_tankunming@outlook.com>
2020-10-11 23:05:47 +0800

    feature: 修改table组件

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'antd';
 import './table.less';
@@ -22,42 +22,49 @@ function paginationReducer(state, action) {
       state.current = payload;
       return state;
     default:
-      throw new Error({ message: 'table组件的useReducer使用了不在定义范围内的Key~' });
+      throw new Error({
+        message: 'table组件的useReducer使用了不在定义范围内的Key~',
+      });
   }
-};
+}
 
 function TableComp(props) {
   // 传入参数
   const { requestFunc, requestQuery, extraPagination } = props;
   // 分页器useReducer
-  const [pagination, paginationDispatch] = useReducer(paginationReducer, initPagination);
+  const [pagination, paginationDispatch] = useReducer(
+    paginationReducer,
+    initPagination
+  );
   // 列表数据
   const [listData, setListData] = useState([]);
   // 加载状态
-  const [loading, setLoading] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   /**
    * DidMount
    */
   useEffect(() => {
-    init()
+    init();
   }, []);
 
   /**
    * 初始化
    */
   function init() {
-    console.log('init');
-    requestData({ current: 1, pageSize: 10 });
-  }
+    requestData({ current: 1, pageSize: 10 }).then(() => {
+      props.onListChange && props.onListChange(pagination, loading);
+    });
+  };
 
   /**
    * refresh
    */
   function refreshList() {
-    console.log('refresh');
-    requestData();
-  }
+    requestData().then(() => {
+      props.onListChange && props.onListChange(pagination, loading);
+    });
+  };
 
   /**
    * 切换页码
@@ -65,9 +72,10 @@ function TableComp(props) {
    * @param {number} pageSize
    */
   function handlePageChange(current, pageSize) {
-    console.log('pageChange');
-    requestData({ current, pageSize })
-  }
+    requestData({ current, pageSize }).then(() => {
+      props.onListChange && props.onListChange(pagination, loading);
+    });
+  };
 
   /**
    * 请求数据
@@ -76,8 +84,8 @@ function TableComp(props) {
    * @returns<Promise>
    */
   async function requestData({ current, size } = {}) {
-    console.log('request')
-    if(loading) return Promise.resolve();
+    if (!props.validateFunc || !props.validateFunc()) return Promise.reject();
+    if (loading) return Promise.resolve();
     setLoading(true);
     try {
       const pageNum = current || pagination.current || 1;
@@ -91,15 +99,15 @@ function TableComp(props) {
       setListData(result.data || []);
       paginationDispatch({
         type: reducerType.CHANGE_PAGE,
-        payload: pageNum
+        payload: pageNum,
       });
       return Promise.resolve(result);
-    } catch(e) {
+    } catch (e) {
       return Promise.reject(e);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   return (
     <Table
@@ -108,12 +116,12 @@ function TableComp(props) {
       pagination={{
         ...pagination,
         ...extraPagination,
-        onChange: handlePageChange
-       }}
+        onChange: handlePageChange,
+      }}
       {...props}
     />
-  )
-};
+  );
+}
 
 TableComp.defaultProps = {
   // 请求function
@@ -122,12 +130,18 @@ TableComp.defaultProps = {
   requestQuery: {},
   // 分页器的其他配置
   extraPagination: {},
+  // 校验方法，默认通过验证
+  validateFunc: () => true,
+  // TableList数据更新回调
+  onListChange: () => {},
 };
 
 TableComp.propTypes = {
   requestFunc: PropTypes.func.isRequired,
   requestQuery: PropTypes.object,
   extraPagination: PropTypes.object,
+  validateFunc: PropTypes.func,
+  onListChange: PropTypes.func,
 };
 
-export default TableComp
+export default TableComp;
