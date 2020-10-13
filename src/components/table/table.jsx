5e8c86fe8be3baf59481d01c992e7029761fb1e0
kunming100 <kim_tankunming@outlook.com>
2020-10-13 16:08:13 +0800

    feature:优化table的逻辑，增加必传参数rowKey。

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -29,7 +29,7 @@ function paginationReducer(state, action) {
 
 function TableComp(props) {
   // 传入参数
-  const { requestFunc, requestQuery, extraPagination } = props;
+  const { rowKey, requestFunc, requestQuery, extraPagination } = props;
   // 分页器useReducer
   const [pagination, paginationDispatch] = useReducer(
     paginationReducer,
@@ -39,17 +39,15 @@ function TableComp(props) {
   // 列表数据
   const [listData, setListData] = useState([]);
   useEffect(() => {
-    console.log('listData改变了', pagination)
     props.onListChange && props.onListChange(pagination);
   }, [listData]);
 
   // 加载状态
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    console.log('loading改变了', loading)
     props.onLoadingChange && props.onLoadingChange(loading);
   }, [loading]);
-  
+
   /**
    * DidMount
    */
@@ -61,21 +59,21 @@ function TableComp(props) {
    * 初始化
    */
   function init() {
-    requestData({ current: 1, pageSize: 10 })
-      // .then(() => {
-      //   props.onListChange && props.onListChange({ pagination, loading });
-      // });
-  };
+    requestData({ current: 1, pageSize: 10 });
+    // .then(() => {
+    //   props.onListChange && props.onListChange({ pagination, loading });
+    // });
+  }
 
-  // /**
-  //  * refresh
-  //  */
-  // function refreshList() {
-  //   requestData()
-  //    .then(() => {
-  //      props.onListChange && props.onListChange({ pagination, loading });
-  //    });
-  // };
+  /**
+   * refresh
+   */
+  function refreshList() {
+    requestData()
+    //  .then(() => {
+    //    props.onListChange && props.onListChange({ pagination, loading });
+    //  });
+  };
 
   /**
    * 切换页码
@@ -83,11 +81,26 @@ function TableComp(props) {
    * @param {number} pageSize
    */
   function handlePageChange(current, pageSize) {
-    requestData({ current, pageSize })
-      // .then(() => {
-      //   props.onListChange && props.onListChange({ pagination, loading });
-      // });
-  };
+    requestData({ current, pageSize });
+    // .then(() => {
+    //   props.onListChange && props.onListChange({ pagination, loading });
+    // });
+  }
+
+  /**
+   * 表单参数命名转换
+   * @param {object} sourceData 源数据
+   * @param {object} transformMap 对象key转换规则，形如{ aB: 'AB' }，将{ aB: 'value' }转换成{ AB: 'value' }
+   */
+  function transformData(sourceData = {}, transformMap = {}) {
+    if (Object.keys(transformMap).length === 0) return sourceData;
+    const formData = {};
+    for (const [key, value] of Object.entries(sourceData)) {
+      const k = transformMap[key] || key;
+      formData[k] = value;
+    }
+    return formData;
+  }
 
   /**
    * 请求数据
@@ -100,40 +113,55 @@ function TableComp(props) {
     if (loading) return Promise.resolve();
     setLoading(true);
     try {
+      const { unNeedPaging: unNeedPaging = false } = extraPagination;
       const pageNum = current || pagination.current || 1;
       const pageSize = size || pagination.pageSize || 10;
-      const params = {
-        ...requestQuery,
-        pageNum,
-        pageSize,
-      };
+      const sourceParams = !unNeedPaging
+        ? {
+            ...requestQuery,
+            current: pageNum,
+            pageSize,
+          }
+        : requestQuery;
+      // 修改参数Key值
+      const params = transformData(sourceParams, props.queryTransformMap);
       const result = await requestFunc(params);
+      if (unNeedPaging) {
+        setListData(result.data || []);
+        return Promise.resolve(result);
+      }
+      const { list = [], pageNo, totalCount = 0 } = result.data;
       paginationDispatch({
         type: reducerType.UPDATE_PAGINATION,
         payload: {
-          current: pageNum,
-          total: result.total
+          current: pageNo,
+          total: totalCount,
         },
       });
-      setListData(result.data || []);
+      setListData(list || []);
       return Promise.resolve(result);
     } catch (e) {
       return Promise.reject(e);
     } finally {
       setLoading(false);
     }
-  };
+  }
 
   return (
     <>
       <Table
+        rowKey={rowKey}
         dataSource={listData}
         loading={loading}
-        pagination={{
-          ...pagination,
-          ...extraPagination,
-          onChange: handlePageChange,
-        }}
+        pagination={
+          !extraPagination.unNeedPaging
+            ? {
+                ...pagination,
+                ...extraPagination,
+                onChange: handlePageChange,
+              }
+            : false
+        }
         {...props}
       />
     </>
@@ -141,10 +169,14 @@ function TableComp(props) {
 }
 
 TableComp.defaultProps = {
+  // 表格的rowKey
+  rowKey: '',
   // 请求function
   requestFunc: () => {},
   // 请求参数
   requestQuery: {},
+  // 请求参数转换映射
+  queryTransformMap: {},
   // 分页器的其他配置
   extraPagination: {},
   // 校验方法，默认通过验证
@@ -156,8 +188,10 @@ TableComp.defaultProps = {
 };
 
 TableComp.propTypes = {
+  rowKey: PropTypes.string.isRequired,
   requestFunc: PropTypes.func.isRequired,
   requestQuery: PropTypes.object,
+  queryTransformMap: PropTypes.object,
   extraPagination: PropTypes.object,
   validateFunc: PropTypes.func,
   onListChange: PropTypes.func,
